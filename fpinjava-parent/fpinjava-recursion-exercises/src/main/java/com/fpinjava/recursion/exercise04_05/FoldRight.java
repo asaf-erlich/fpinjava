9dc89a891a282918501f831bfe1e81058879d24e
Asaf Erlich <asaf.erlich@gmail.com>
2017-09-27 07:46:09 -0700

    exercise04_05

--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_05/FoldRight.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_05/FoldRight.java
@@ -3,10 +3,19 @@ package com.fpinjava.recursion.exercise04_05;
 import java.util.List;
 
 import com.fpinjava.common.Function;
+import com.fpinjava.common.TailCall;
+
+import static com.fpinjava.common.CollectionUtilities.*;
 
 public class FoldRight {
 
   public static <T, U> U foldRight(List<T> ts, U identity, Function<T, Function<U, U>> f) {
-    throw new RuntimeException("To be implemented.");
+    return foldRightHelper(reverse(ts), identity, f).eval();
+  }
+
+  public static <T, U> TailCall<U> foldRightHelper(List<T> ts, U identity, Function<T, Function<U, U>> f) {
+    if (ts.isEmpty()) return TailCall.ret(identity);
+
+    return TailCall.sus(() -> foldRightHelper(tail(ts), f.apply(head(ts)).apply(identity), f));
   }
 }
