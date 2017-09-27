ae4570c86b1bac07fd7c072515460ff915accdd0
Asaf Erlich <asaf.erlich@gmail.com>
2017-09-27 07:35:26 -0700

    exercise04_04

--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_04/Range.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_04/Range.java
@@ -1,11 +1,22 @@
 package com.fpinjava.recursion.exercise04_04;
 
-import java.util.List;
+import com.fpinjava.common.TailCall;
+
+import java.util.*;
+
+import static com.fpinjava.common.CollectionUtilities.append;
+import static com.fpinjava.common.CollectionUtilities.list;
 
 
 public class Range {
 
   public static List<Integer> range(Integer start, Integer end) {
-    throw new RuntimeException("To be implemented.");
+    return rangeHelper(start, end, list()).eval();
+  }
+
+  private static TailCall<List<Integer>> rangeHelper(final Integer start, final Integer end, final List<Integer> list) {
+    if (start >= end) return TailCall.ret(list);
+
+    return TailCall.sus(() -> rangeHelper(start + 1, end, append(list, start)));
   }
 }
