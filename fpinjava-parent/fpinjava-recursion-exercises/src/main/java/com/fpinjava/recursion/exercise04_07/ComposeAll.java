1944c12aa74c91158b1c08574dda1433faa17a6b
Asaf Erlich <asaf.erlich@gmail.com>
2017-09-27 21:00:43 -0700

    exercise04_07

--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_07/ComposeAll.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_07/ComposeAll.java
@@ -4,16 +4,27 @@ import java.util.List;
 
 import com.fpinjava.common.Function;
 
-public class ComposeAll {
+import static com.fpinjava.recursion.exercise04_03.FoldLeft.foldLeft;
+import static com.fpinjava.recursion.exercise04_05.FoldRight.foldRight;
+
+  public class ComposeAll {
 
   public static <T> Function<T, T> composeAll(List<Function<T, T>> list) {
-    throw new RuntimeException("To be implemented.");
+    return identity -> {
+      T result = identity;
+
+      for (Function<T, T> f : list) {
+        result = f.apply(result);
+      }
+
+      return result;
+    };
   }
   public static <T> Function<T, T> composeAllViaFoldLeft(List<Function<T, T>> list) {
-    throw new RuntimeException("To be implemented.");
+    return identity -> foldLeft(list, identity, f1 -> f2 -> f2.apply(f1));
   }
 
   public static <T> Function<T, T> composeAllViaFoldRight(List<Function<T, T>> list) {
-    throw new RuntimeException("To be implemented.");
+    return identity -> foldRight(list, identity, f -> f::apply);
   }
 }
