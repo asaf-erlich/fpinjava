75d661325376a11235e5f1a7d3c8f2e310097f3d
Asaf Erlich <asaf.erlich@gmail.com>
2017-10-08 20:30:29 -0700

    5_07

--- fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_07/Sum.java
+++ fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_07/Sum.java
@@ -5,6 +5,12 @@ import com.fpinjava.lists.exercise05_06.List;
 public class Sum {
 
   public static Integer sum(List<Integer> ints) {
-    throw new RuntimeException("To be implemented");
+    return sum(0, ints);
+  }
+
+  private static Integer sum(final int accumulator, final List<Integer> ints) {
+    if (ints.isEmpty()) return accumulator;
+
+    return sum(accumulator + ints.head(), ints.tail());
   }
 }
