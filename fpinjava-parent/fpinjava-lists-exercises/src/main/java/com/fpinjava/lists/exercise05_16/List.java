12aab5b3c3ab6e2ccc11cc5245e34f526056f132
Asaf Erlich <asaf.erlich@gmail.com>
2017-10-09 17:24:08 -0700

    5_16

--- fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_16/List.java
+++ fpinjava-parent/fpinjava-lists-exercises/src/main/java/com/fpinjava/lists/exercise05_16/List.java
@@ -205,6 +205,6 @@ public abstract class List<A> {
   }
 
   public static <A> List<A> flatten(List<List<A>> list) {
-    throw new IllegalStateException("To be implemented");
+    return list.foldLeft(List.list(), acc -> listOfA -> concat(acc, listOfA));
   }
 }
